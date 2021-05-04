e50e6a42874a966d4975ec7ed81e47a43f400549
schikkinikki <kunk.nik@googlemail.com>
2021-05-04 19:24:36 +0200

    reworking Strafen logic

--- KegelApp/lib/models/MembersListClass.dart
+++ KegelApp/lib/models/MembersListClass.dart
@@ -33,100 +33,6 @@ class MemberListClass extends ChangeNotifier {
     return memberlist.firstWhere((element) => element.name == name);
   }
 
-  // void setStrafen(int index, String strafen) {
-  //   memberlist.forEach((player) {
-  //     List<int> strafenliste = player.strafenListen;
-  //     while (player.isSelected == 1) {
-  //       switch (strafen) {
-  //         case "Pumpe":
-  //           strafenliste[index]++;
-  //           player.setPumpe(strafenliste[index]);
-  //           print(player.name + player.pumpen.toString());
-  //           break;
-  //         case "Klingeln":
-  //           strafenliste[index]++;
-  //           player.setKlingeln(strafenliste[index]);
-  //           break;
-  //         case "Stina":
-  //           strafenliste[index]++;
-  //           player.setMittenDurch(strafenliste[index]);
-  //           break;
-  //         case "Durchwurf":
-  //           strafenliste[index]++;
-  //           player.setDurchWurf(strafenliste[index]);
-  //           break;
-  //         case "Handy":
-  //           strafenliste[index]++;
-  //           player.setHandy(strafenliste[index]);
-  //           break;
-  //         case "Kugel bringen":
-  //           strafenliste[index]++;
-  //           player.setKugelBringen(strafenliste[index]);
-  //           break;
-  //         case "Zu 2. auf der Bahn":
-  //           strafenliste[index]++;
-  //           player.setZuZweitaufDerBahn(strafenliste[index]);
-  //           break;
-  //         case "Lustwurf":
-  //           strafenliste[index]++;
-  //           player.setLustwurf(strafenliste[index]);
-  //           break;
-  //         default:
-  //           break;
-  //       }
-  //       return;
-  //     }
-  //   });
-  //   notifyListeners();
-  // }
-
-  void setStrafenLower(int index, String strafen) {
-    memberlist.forEach((player) {
-      List<int> strafenliste = player.strafenListen;
-      while (player.isSelected == 1) {
-        switch (strafen) {
-          case "Pumpe":
-            strafenliste[index]--;
-            player.setPumpe(strafenliste[index]);
-            print(player.name + player.pumpen.toString());
-            break;
-          case "Klingeln":
-            strafenliste[index]--;
-            player.setKlingeln(strafenliste[index]);
-            break;
-          case "Stina":
-            strafenliste[index]--;
-            player.setMittenDurch(strafenliste[index]);
-            break;
-          case "Durchwurf":
-            strafenliste[index]--;
-            player.setDurchWurf(strafenliste[index]);
-            break;
-          case "Handy":
-            strafenliste[index]--;
-            player.setHandy(strafenliste[index]);
-            break;
-          case "Kugel bringen":
-            strafenliste[index]--;
-            player.setKugelBringen(strafenliste[index]);
-            break;
-          case "Zu 2. auf der Bahn":
-            strafenliste[index]--;
-            player.setZuZweitaufDerBahn(strafenliste[index]);
-            break;
-          case "Lustwurf":
-            strafenliste[index]--;
-            player.setLustwurf(strafenliste[index]);
-            break;
-          default:
-            break;
-        }
-        return;
-      }
-    });
-    notifyListeners();
-  }
-
   int getOneStrafe(int index) {
     Kegelbruder player = memberlist.firstWhere(
         (element) => element.isSelected == 1,
@@ -134,7 +40,7 @@ class MemberListClass extends ChangeNotifier {
     return player.getSingleStrafe(index);
   }
 
-// set Strafen for selected Player
+// set Strafen for selected Player higher
   void setStrafen(int index, String strafen) async {
     // get all Kegelbruder from DB
     List<Kegelbruder> kegelbruderListe =
@@ -157,10 +63,100 @@ class MemberListClass extends ChangeNotifier {
             break;
           case "Stina":
             strafenListe[index]++;
-          // TODO finish logic here
+            kegelbruder.setMittenDurch(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          case "Durchwurf":
+            strafenListe[index]++;
+            kegelbruder.setDurchWurf(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          case "Handy":
+            strafenListe[index]++;
+            kegelbruder.setHandy(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          case "Kugel bringen":
+            strafenListe[index]++;
+            kegelbruder.setKugelBringen(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          case "Zu 2. auf der Bahn":
+            strafenListe[index]++;
+            kegelbruder.setZuZweitaufDerBahn(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          case "Lustwurf":
+            strafenListe[index]++;
+            kegelbruder.setLustwurf(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          default:
+            break;
         }
         return;
       }
+      notifyListeners();
+    });
+  }
+
+  // set Strafen for selected Player lower
+  void setStrafenLower(int index, String strafen) async {
+    // get all Kegelbruder from DB
+    List<Kegelbruder> kegelbruderListe =
+        await DBProvider.db.getAllKegelbruder();
+
+    // get strafenliste from kegelbruder an loop through until he is // no longer selected
+    kegelbruderListe.forEach((kegelbruder) {
+      List<int> strafenListe = kegelbruder.strafenListen;
+      while (kegelbruder.isSelected == 1) {
+        switch (strafen) {
+          case "Pumpe":
+            strafenListe[index]--;
+            kegelbruder.setPumpe(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          case "Klingeln":
+            strafenListe[index]--;
+            kegelbruder.setKlingeln(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          case "Stina":
+            strafenListe[index]--;
+            kegelbruder.setMittenDurch(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          case "Durchwurf":
+            strafenListe[index]--;
+            kegelbruder.setDurchWurf(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          case "Handy":
+            strafenListe[index]--;
+            kegelbruder.setHandy(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          case "Kugel bringen":
+            strafenListe[index]--;
+            kegelbruder.setKugelBringen(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          case "Zu 2. auf der Bahn":
+            strafenListe[index]--;
+            kegelbruder.setZuZweitaufDerBahn(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          case "Lustwurf":
+            strafenListe[index]--;
+            kegelbruder.setLustwurf(strafenListe[index]);
+            DBProvider.db.updateKegelbruder(kegelbruder);
+            break;
+          default:
+            break;
+        }
+        return;
+      }
+      notifyListeners();
     });
   }
 }
